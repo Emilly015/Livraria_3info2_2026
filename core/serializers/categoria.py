9be1326d35a62d5009df292c62e975d9b17708cd
emilly015 <emilly.v.duarte01@gmail.com>
2026-03-16 16:47:29 -0300

    feat: refazendo projeto e alterando dados

---
+++ core/serializers/categoria.py
@@ -0,0 +1,9 @@
+from attr import fields
+from core.models import Categoria
+from rest_framework.serializers import ModelSerializer
+
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
