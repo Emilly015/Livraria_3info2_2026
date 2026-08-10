f928d15524c524a480a23dc296f304dacd0a24c8
emilly015 <emilly.v.duarte01@gmail.com>
2026-08-10 15:07:29 -0300

    feat: criação dos itens da compra

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,7 @@ from django.contrib.admin import ModelAdmin, register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Autor, Categoria, Compra, Editora, Livro, User
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro, User
 
 
 @register(Autor)
@@ -40,6 +40,13 @@ class EditoraAdmin(ModelAdmin):
     search_fields = ('nome', 'email', 'cidade')
     list_filter = ('nome', 'email', 'cidade')
     ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+
+@register(ItensCompra)
+class ItensCompraAdmin(ModelAdmin):
+    list_display = ('compra', 'livro', 'quantidade')
+    ordering = ('compra', 'livro', 'quantidade')
     list_per_page = 10
 
 
